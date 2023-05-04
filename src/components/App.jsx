b2c2c842baef6eecd6114b2f36267ce23833e019
Djoult <55453354+Djoult@users.noreply.github.com>
2023-05-04 13:42:30 +0300

    Add statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import { Profile } from './Profile/Profile';
 import user from './Profile/user.json';
 
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/data.json';
+
 export const App = () => {
   return (
     <>
@@ -11,6 +14,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            key={id}
+            className={css.item}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array.isRequired,
+  id: PropTypes.string,
+  label: PropTypes.string,
+  percentage: PropTypes.number,
+};
